991451f0bd14b6b20a72903b499daa76d0ee991b
Bartlomiej2001 <57993203+Bartlomiej2001@users.noreply.github.com>
2020-10-27 14:49:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>crud-wiele-do-wielu-bartlomiej-basinski</title>
     <link rel="stylesheet" href="style.css">
 <body>
     <header><h1>header</h1></header>
