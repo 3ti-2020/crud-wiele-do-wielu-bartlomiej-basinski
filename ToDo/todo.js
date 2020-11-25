a0d7241b3d23c8a478286e12e7557f4859af870e
Bartlomiej2001 <57993203+Bartlomiej2001@users.noreply.github.com>
2020-11-25 09:14:31 +0100

    Add files via upload

---
+++ ToDo/todo.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const todoList = document.querySelector("#todoList");
+    const todoForm = document.querySelector("#todoForm");
+    const todoSearch = document.querySelector("#todoSearch");
+    const todoTextarea = todoForm.querySelector('#todoMessage');
+    const list = document.querySelector('#list');
+
+    function addTask(text) {
+        list.innerHTML += "<h1>" + todoTextarea.value + "<h1><br>";
+        
+    }
+
+    todoForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        if (todoTextarea.value !== "") {
+            addTask(todoTextarea.value);
+            todoTextarea.value = "";
+        }
+    });
+});
