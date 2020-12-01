8ae837a6bb143ed9ccd7e75d47ae6323c6e97578
Bartlomiej2001 <57993203+Bartlomiej2001@users.noreply.github.com>
2020-12-01 14:19:23 +0100

    Add files via upload

--- Pytania.php
+++ Pytania.php
@@ -12,7 +12,7 @@
 <header>
 <a class="karty" href="index.php">logowanie</a>
 <div><h1>Bartłomiej Basiński gr2</h1></div>
-
+<a class="karty" href="Pytania.php">pytania</a>
 </header>
 
 <aside>
@@ -20,20 +20,41 @@
 <main>
 <div class="pytanie">
 <h1>EE.09-SG-20.01</h1>
-<h3>Z9.DOM dostarcza metod i własności, które w języku JavaScript pozwalają na</h3>
-<p>A. manipulowanie zadeklarowanymi w kodzie łańcuchami.</p>
-<p>B. wysłanie danych formularza bezpośrednio do bazy danych.</p>
-<p>C. wykonywanie operacji na zmiennych przechowujących liczby</p>
-<p><h4>D. pobieranie i modyfikowanie elementów strony wyświetlonej przez przeglądarkę.</h4></p>
+<article class="question">
+    <h3 class="question_title">Z9. DOM dostarcza metod i własności, które w języku JavaScript pozwalają na</h3>
+    <ol type="a" class="question_answerList">
+      <li class="odp">manipulowanie zadeklarowanymi w kodzie łańcuchami.</li>
+      <li class="odp">wysłanie danych formularza bezpośrednio do bazy danych.</li>
+      <li class="odp">wykonywanie operacji na zmiennych przechowujących liczby</li>
+      <li class="odp">pobieranie i modyfikowanie elementów strony wyświetlonej przez przeglądarkę.</li>
+      <details class="question_details">
+        <summary class="odp">sprawdź odpowiedź</summary> 
+        <p class="pop"> odpowiedz: 
+          <span class="pop">d</span> 
+        </p>
+      </details>
+    </ol>
+  </article>
 </div>
 <div class="pytanie">
 <h1>EE.09-SG-20.01</h1>
-<h3>Witryna internetowa ma mieć przedstawioną strukturę bloków. Aby uzyskać ten układ, należy znacznikom
-sekcji przypisać właściwości w następujący sposób: </h3>
-<p>A. float tylko dla bloku 5; clear dla bloku 2</p>
-<p>B. float tylko dla bloków: 3, 4; clear dla bloku 5</p>
-<p>C. float tylko dla bloku 2; clear dla bloków: 3, 4</p>
-<p><h4>D. float tylko dla bloków: 2, 3, 4; clear dla bloku 5</h4></p>
+<img src="obrzad/zadanie26.png">
+<article class="question">
+    <h3 class="question_title">Z26. Witryna internetowa ma mieć przedstawioną strukturę bloków. Aby uzyskać ten układ, należy znacznikom
+sekcji przypisać właściwości w następujący sposób:</h3>
+    <ol type="a" class="question_answerList">
+      <li class="odp">float tylko dla bloku 5; clear dla bloku 2</li>
+      <li class="odp">float tylko dla bloków: 3, 4; clear dla bloku 5</li>
+      <li class="odp">float tylko dla bloku 2; clear dla bloków: 3, 4</li>
+      <li class="odp">float tylko dla bloków: 2, 3, 4; clear dla bloku 5</li>
+      <details class="question_details">
+        <summary class="odp">sprawdź odpowiedź</summary> 
+        <p class="pop"> odpowiedz: 
+          <span class="pop">d</span> 
+        </p>
+      </details>
+    </ol>
+  </article>
 </div>
 
 </main>
